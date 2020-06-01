b25891e26161040b9e9c19695db7e693a0de5c49
Argyris <arapo4@gmail.com>
2020-06-01 17:02:37 +0200

    Multiple images for animate | multiple isosurfaces

--- isoSURF.py
+++ isoSURF.py
@@ -20,25 +20,25 @@ data_dir = f'/gpfsscratch/rech/avl/username/path/to/data/folder'
 grid_dir = f'/gpfsscratch/rech/avl/username/path/to/grid/folder'
 
 # Data and Grids file names
-data_fname = f'Q_small_field.nc'
+data_fname = f're550_016000vel_der_eps.nc'
 grid_fname = f'grid.nc'
 
 # Image directory and name
 imag_dir = f'/gpfsscratch/rech/avl/username/path/to/image/folder'
-imag_name = 'test_imamge.png'
+imag_name = f'test_image.png'
 
 # Fields to Load from netcdf as they appear in the netcdf file
 # If we want a grid coordinate to load as a scalar variable add to the list
 # 'x','y', or 'z'
-fields = 'Q,x'
+fields = 'u,y'
 
 # Grids variables names as they appear in the netcdf file
 grid_var_names = 'gridx,gridy,gridz'
 
 # Size of fields to load (nx,ny,nz)
-nx = 500
-ny = 500
-nz = 500
+nx = 800
+ny = 193
+nz = 700
 
 
 # Read Grid file - no changes needed
@@ -51,16 +51,21 @@ f.close()
 del f
 # ------------
 
+# Time evolution settings
+nt = 1          # number of saved timesteps
+istart = 2200   # Iteration number of first saved time step
+istep = 50      # Number of iterations per saved time step
 
-# Isosurface Settings
-isosurface_field = 'Q'
-isovalue = 0.04
-scalar_to_color = 'x' # Scalar field to color the isosurface
+# Isosurface Settings.
+# For multiple isosurfaces just add entries to all the  corresponding arrays.
+isosurface_fields = ['u','u']
+isovalue = [-0.2,0.2]
+scalar_to_color = ['y','y'] # Scalar field to color the isosurface
 
 # Colormap
-cmap = 'Blues'
-cmap_min = x[0]  # min value color
-cmap_max = x[-1] # max value color
+cmap = ['Blues','Blues']
+cmap_min = [y[0], y[0]]  # min value color
+cmap_max = [y[120], y[120]] # max value color
 
 # Create a plane for bottom wall(0: False, 1: True)
 wall = 1
@@ -87,50 +92,69 @@ raytracing = 0
 # =============================================================================
 if __name__ == '__main__':
 
-    # Compute Bounds of Grid
-    startx,starty,startz,midx,midy,midz,endx,endy,endz = compute_bounds(x,y,z)
-
-    renderView1 = setup_render_view(startx,midx,midy,endy,midz,endz,
-                                    cx,cy,cz,cps,resolution,raytracing)
-
     # Load Plugins
     LoadPlugin(f'{plugin_dir}/netcdfSource.py', ns=globals())
 
+    for it in range(nt):
 
-    # create a new 'netcdf Source'
-    netcdfSource1 = netcdfSource(DataFileName=f'{data_dir}/{data_fname}',
-                                GridFileName=f'{grid_dir}/{grid_fname}')
+        # If multiple time steps change the name of netcdf file to open
+        # and the name of image to be saved
+        if (nt > 1):
+            int2char = str(it*istep+istart).zfill(6) # Timestep of file to open
+            data_fname = f're550_{int2char}vel_der_eps.nc'
+            imag_name = f'test_image_{i}.png'
 
-    netcdfSource1.FieldsToLoad = fields
-    netcdfSource1.VariableNameOfGridsInNetcdfFile = grid_var_names
-    netcdfSource1.nx = nx
-    netcdfSource1.ny = ny
-    netcdfSource1.nz = nz
+        # Compute Bounds of Grid
+        startx,starty,startz,midx,midy,midz,endx,endy,endz = compute_bounds(x,y,z)
 
-    # Create Isosurface
-    contour1 = Contour(Input=netcdfSource1)
-    contour1.ContourBy = ['POINTS', isosurface_field]
-    contour1.Isosurfaces = [isovalue]
-    contour1.PointMergeMethod = 'Uniform Binning'
-
-    # Settings for Isosurface
-    isosurface_settings(contour1, renderView1, isosurface_field,
-                        scalar_to_color, cmap, cmap_min, cmap_max, raytracing)
-
-    # create a new 'Plane'
-    if wall:
-        plane1 = Plane()
-        plane1.Origin = [x[0], y[0], z[0]]
-        plane1.Point1 = [x[0], y[0], z[-1]]
-        plane1.Point2 = [x[-1], y[0], z[0]]
-
-        plane_settings(plane1, renderView1)
+        renderView1 = setup_render_view(startx,midx,midy,endy,midz,endz,
+                                        cx,cy,cz,cps,resolution,raytracing)
 
 
-    SetActiveSource(contour1)
 
-    # Save Screenshot
-    SaveScreenshot(f'{imag_dir}/{imag_name}', ImageResolution=(resolution[0],
-                                                               resolution[1]),
-                TransparentBackground = 1,
-                CompressionLevel = 0)
+        # create a new 'netcdf Source'
+        netcdfSource1 = netcdfSource(DataFileName=f'{data_dir}/{data_fname}',
+                                    GridFileName=f'{grid_dir}/{grid_fname}')
+
+        netcdfSource1.FieldsToLoad = fields
+        netcdfSource1.VariableNameOfGridsInNetcdfFile = grid_var_names
+        netcdfSource1.nx = nx
+        netcdfSource1.ny = ny
+        netcdfSource1.nz = nz
+
+        # Create Isosurfaces
+        contour[]
+        for is in range(isosurface_fields.size)
+            contour.append(Contour(Input=netcdfSource1))
+            contour[is].ContourBy = ['POINTS', isosurface_fields[is]]
+            contour[is].Isosurfaces = [isovalue[is]]
+            contour[is].PointMergeMethod = 'Uniform Binning'
+
+            # Settings for Isosurface
+            isosurface_settings(contour[is], renderView1, isosurface_fields[is],
+                                scalar_to_color[is], cmap[is], cmap_min[is],
+                                cmap_max[is], raytracing)
+
+        # create a new 'Plane'
+        if wall:
+            plane1 = Plane()
+            plane1.Origin = [x[0], y[0], z[0]]
+            plane1.Point1 = [x[0], y[0], z[-1]]
+            plane1.Point2 = [x[-1], y[0], z[0]]
+
+            plane_settings(plane1, renderView1)
+
+
+        SetActiveSource(contour[0])
+
+        # Save Screenshot
+        SaveScreenshot(f'{imag_dir}/{imag_name}',ImageResolution=(resolution[0],
+                                                                resolution[1]),
+                        TransparentBackground = 1,
+                        CompressionLevel = 0)
+
+        # If multiple time steps need to be opened
+        # reset session to open new files
+        if nt>1:
+            Disconnect()
+            Connect()
